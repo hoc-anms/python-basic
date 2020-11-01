9be2ea0fd54ca9d41e33fbaa3c5bcfa136aeabad
hoc-anms <phamvanhoc96@gmail.com>
2020-11-01 23:38:55 +0700

    first program

---
+++ ex/ex1_first_program.py
@@ -0,0 +1,22 @@
+# First Program
+
+# With SYNTAX OF PYTHON2
+
+#print "Hello World!"
+#print "Hello Again"
+#print "I like typing this."
+#print "This is fun."
+#print 'Yay! Printing.'
+#print "I'd much rather you 'not'."
+#print 'I "said" do not touch this.'
+
+
+#With SYNTAX OF PYTHON3
+
+print ("Hello World!")
+print ("Hello Again")
+print ("I like typing this.")
+print ("This is fun.")
+print ('Yay! Printing.')
+print ("I'd much rather you 'not'.")
+print ('I "said" do not touch this.')
